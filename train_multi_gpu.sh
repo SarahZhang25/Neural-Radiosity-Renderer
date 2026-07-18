9e0529babd82ced077f8300770a56f77502c1c17
Sarah Zhang <sazhang@mit.edu>
2026-07-18 15:51:07 -0400

    Update configs

--- train_multi_gpu.sh
+++ train_multi_gpu.sh
@@ -3,8 +3,7 @@
 # Basic usage (defaults to 2 GPUs)
 # ./train_multi_gpu.sh
 # Specifying the number of GPUs and a custom config
-# ./train_multi_gpu.sh 4 training/train_config_46M.yaml
-# ./train_multi_gpu.sh 8 training/train_config_46M_h5_test.yaml
+# ./train_multi_gpu.sh 4 training/train_config_46M_h5.yaml
 
 # Default to 2 GPUs if not specified
 NUM_GPUS=${1:-2}
@@ -17,7 +16,7 @@ source /home/sazhang/miniconda3/etc/profile.d/conda.sh
 conda activate "$CONDA_ENV_NAME"
 
 # Construct CUDA_VISIBLE_DEVICES (e.g., 0,1 for 2 GPUs)
-# export CUDA_VISIBLE_DEVICES=5,6
+# export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 echo "Starting multi-GPU training on $NUM_GPUS GPUs (CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES) using config $CONFIG..."
 python -m torch.distributed.run --nproc_per_node=$NUM_GPUS training/train.py --config $CONFIG
